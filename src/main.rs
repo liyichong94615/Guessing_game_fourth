b49690094c03effd72ab8c4289e040803c585901
liyichong94615 <liyichong94@gmail.com>
2021-08-07 00:51:28 +0800

    Fourth Guess updated-2

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 extern crate rand;
+
 use std::io;
-use rand::Rng;
 use std::cmp::Ordering;
+use rand::Rng;
 
 fn main(){
     println!("Guess the number!");
+
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
-    println!("Please input your guess");
+    println!("The secret number is {}", secret_number);
+    println!("please input your guess.");
+
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
+    let guess: u32 = guess.trim().parse().expect("please type a number");
     println!("You guessed: {}", guess);
     match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Greater=>println!("Too Big!"),
-        Ordering::Equal=>println!("You Win!"),
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win"),
     }
-
 }
